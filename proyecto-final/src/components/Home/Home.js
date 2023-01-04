2b8729d16980c13e9bb072f5a09ac148d706fb94
Ignacio Garcés <ignaciogarces96@gmail.com>
2023-01-04 02:50:41 -0300

    first commit

---
+++ proyecto-final/src/components/Home/Home.js
@@ -0,0 +1,20 @@
+import './Home.css'
+
+
+
+
+export default function Home() {
+    return(
+        <div className='main-container'>
+        <header className='main'>
+            <h1>Proyecto - Rick & Morty</h1>
+
+            <div className='bt-container'>
+            <button className='home-bt'>Characters</button>
+            <button className='home-bt'>Contact</button>
+            </div>
+        </header>
+
+        </div>
+    )
+}
